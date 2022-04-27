b3090447e9f3dbd40eff8752db1825852d6be523
a8675309 <a8675309@.unc.edu>
2022-04-26 22:19:17 -0400

    cookies work, sort of.  Need to think about issues with deleting cookies / if a user tries to login twice.  But login is about at the same level of functionality as the register page now

---
+++ public/js/login.js
@@ -0,0 +1,35 @@
+
+
+async function getJson2(myemail, mypassword){
+    const response = await fetch('/login-user',{
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify({
+
+            email: myemail,
+            password: mypassword
+        })})
+    
+    const my_json = await response.json();
+    
+    if (response.ok){
+
+        const token = my_json.loginToken;
+        console.log(my_json.message)
+        //console.log(token)
+        document.cookie = "token=" + token + "; path=/";
+        console.log(document.cookie)
+        window.location = "/html/surveyPage.html"
+    }
+    else{
+        console.log(my_json.message);
+    }
+    
+}
+document.getElementById("login").onclick = function () {
+
+    const myemail= document.getElementById("email").value;
+    const mypassword = document.getElementById("password").value;   
+    getJson2(myemail, mypassword);
+    
+}
