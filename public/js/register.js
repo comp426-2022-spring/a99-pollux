3458987b5559038dcfc6efd61f3a4d1e31156345
a8675309 <a8675309@.unc.edu>
2022-04-26 21:31:24 -0400

    figured out issue from previous commit, will try to look at login now

--- public/js/register.js
+++ public/js/register.js
@@ -1,15 +1,52 @@
 
 
-
+async function getJson(myname, mypassword, myemail){
+    const response = await fetch('/register-user',{
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify({
+            name : myname,
+            email: myemail,
+            password: mypassword
+        })})
+    
+    const my_json = await response.json();
+    
+    if (response.ok){
+        window.location = "/html/loginPage.html";
+    }
+    else{
+        console.log(my_json.message);
+    }
+    //console.log(my_json.message);
+    //console.log(response.ok);
+    //return my_json;
+}
 document.getElementById("submit").onclick = function () {
 
     const myname = document.getElementById("name").value;
     const myemail= document.getElementById("email").value;
     const mypassword = document.getElementById("password").value;   
+    getJson(myname,mypassword,myemail);
+    //console.log(getJson(myname,mypassword,myemail));
 
-    console.log(myname);
-    console.log(mypassword);
-    console.log(myemail);
+
+    // console.log(myname);
+    // console.log(mypassword);
+    // console.log(myemail);
+
+    // const response = await fetch('/register-user',{
+    //     method: 'post',
+    //     headers: new Headers({'Content-Type': 'application/json'}),
+    //     body: JSON.stringify({
+    //         name : myname,
+    //         email: myemail,
+    //         password: mypassword
+    //     })})
+    
+    // const my_json = await response.json();
+    // console.log(my_json);
+    /*
     fetch('/register-user',{
         method: 'post',
         headers: new Headers({'Content-Type': 'application/json'}),
@@ -28,8 +65,9 @@ document.getElementById("submit").onclick = function () {
             //console.log(JSON.stringify(response));
             
             //console.log(Promise.resolve(response.json()))
-            console.log(response.json())
-            window.location = "/html/loginPage.html"
+            console.log(response.json().message)
+            window.location = "/html/loginPage.html";
         }
     })
+    */
 }
